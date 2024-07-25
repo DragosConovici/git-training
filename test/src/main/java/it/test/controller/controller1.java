0dc15799a344a74460dc2c02b5a9ce1be2d96e3c
DragosConovici <conovicidragos00@gmail.com>
2024-07-25 12:00:41 +0200

    added first controller

---
+++ test/src/main/java/it/test/controller/controller1.java
@@ -0,0 +1,13 @@
+@Controller
+@RequestMapping("books")
+public class SimpleBookController {
+
+    @GetMapping("/{id}", produces = "application/json")
+    public @ResponseBody Book getBook(@PathVariable int id) {
+        return findBookById(id);
+    }
+
+    private Book findBookById(int id) {
+        return new Book();
+    }
+}
