f70c1e697cc60048a25116931b915e7841e06f3d
DragosConovici <conovicidragos00@gmail.com>
2024-07-25 12:24:54 +0200

    Modifica utility.java

--- test/src/main/java/it/test/controller/utility.java
+++ test/src/main/java/it/test/controller/utility.java
@@ -4,15 +4,15 @@ public static void main(String[] args) {
 
     // Esempio di concatenazione di stringhe
     String concatenated = concatenateStrings(str1, str2);
-    System.out.println("Concatenated: " + concatenated);
+    System.out.println("Concatenated: " + Concatenated Modificato);
 
     // Esempio di conversione in maiuscolo
     String upperCase = toUpperCase(concatenated);
-    System.out.println("Upper Case: " + upperCase);
+    System.out.println("Upper Case: " + UpperCasa);
 
     // Esempio di verifica se una stringa contiene un'altra stringa
     boolean contains = containsSubstring(upperCase, "MONDO");
-    System.out.println("Contains 'MONDO': " + contains);
+    System.out.println("Contains 'MONDO': " + "MONDO");
 
     // Esempio di sostituzione di una sottostringa
     String replaced = replaceSubstring(upperCase, "CIAO", "HELLO");
