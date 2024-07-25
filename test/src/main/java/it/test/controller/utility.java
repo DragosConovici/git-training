f72794e67cab33b70b54a7a6369e6cb234795421
Alexandru Istrate <alexandru.gabriel.istrate@gmail.com>
2024-07-25 12:35:08 +0200

    modifica file utility.java

--- test/src/main/java/it/test/controller/utility.java
+++ test/src/main/java/it/test/controller/utility.java
@@ -1,22 +1,28 @@
 public static void main(String[] args) {
     String str1 = "Ciao";
     String str2 = " Mondo";
+    String str3 = " Pippo";
 
     // Esempio di concatenazione di stringhe
     String concatenated = concatenateStrings(str1, str2);
-    System.out.println("Concatenated: " + Concatenated Modificato);
+    System.out.println("ConcatenatedNOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!: " + Concatenated Modificato);
 
     // Esempio di conversione in maiuscolo
     String upperCase = toUpperCase(concatenated);
-    System.out.println("Upper Case: " + upperCase);
+    System.out.println("asdasdasd: " + upperCase);
     //maiuscolo e minuscolo
     System.out.println("Upper Case: " + UpperCasa);
 
     // Esempio di verifica se una stringa contiene un'altra stringa prova
     boolean contains = containsSubstring(upperCase, "MONDO");
-    System.out.println("Contains 'MONDO2': " + contains);
+    System.out.println("Contains 'MONDO2312w3': " + contains);
 
     // Esempio di sostituzione di una sottostringa
-    String replaced = replaceSubstring(upperCase, "CIAOBELLA", "HELLO");
+    String replaced = replaceSubstring(upperCase, "CIAOBELLA", "HELasdadsLO");
     System.out.println("Replaced: " + replaced);
+}
+
+// Metodo per concatenare due stringhe
+public static String concatenateStrings(String str1, String str2) {
+    return str1 + str2;
 }
